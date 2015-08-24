03d1df8c776908142fb53dfbb2e823217597230e
Torey Hickman <torey@toreyhickman.com>
2015-08-24 13:06:49 -0500

    Change variable name

--- destroy_message.rb
+++ destroy_message.rb
@@ -1,7 +1,5 @@
-def destroy_message(string)
-  #TODO: return the part a string without the message
+def destroy_message(message)
 end
 
-def destroy_message!(string)
-  #TODO: remove the message from string destructively!
+def destroy_message!(message)
 end
