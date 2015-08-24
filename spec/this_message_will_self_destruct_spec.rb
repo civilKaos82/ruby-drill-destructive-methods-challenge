9449b0786d2a865ea254fd083d5b5c1661ddd695
Torey Hickman <torey@toreyhickman.com>
2015-08-24 12:59:46 -0500

    Refactor tests

--- spec/this_message_will_self_destruct_spec.rb
+++ spec/this_message_will_self_destruct_spec.rb
@@ -1,26 +1,51 @@
 require_relative '../this_message_will_self_destruct'
 
 describe 'destroy_message' do
-  it 'should not modify the provided string' do
-    string = "this message will self-destruct: you can't hug every cat"
-    original_string = string.dup
-    expect(destroy_message(string)).to eq("this message will self-destruct:")
-    expect(string).to eq(original_string)
+  context 'message contains an alert' do
+    let(:message) { 'This message will self-destruct: You can\'t hug every cat.' }
+
+    it 'returns the message prompt' do
+      expect(destroy_message(message)).to eq 'This message will self-destruct:'
+    end
+
+    it 'does not modify its argument' do
+      expect { destroy_message(message) }.to_not change { message }
+    end
   end
 
-  it 'should not modify a string without a message' do
-    string = "this has no message"
-    original_string = string.dup
-    expect(destroy_message(string)).to eq(string)
-    expect(string).to eq(original_string)
+  context 'message does not contain an alert' do
+    it 'returns the whole message' do
+      message = 'This has no alert.'
+      expect(destroy_message(message)).to eq "This has no alert."
+    end
+
+    it 'does not modify its argument' do
+      expect { destroy_message(message) }.to_not change { message }
+    end
   end
 end
 
 describe 'destroy_message!' do
-  it 'should modify the original string' do
-    string = "this message will self-destruct: you can't hug every cat"
-    original_string = string.dup
-    expect(destroy_message!(string)).to eq("this message will self-destruct:")
-    expect(string).to_not eq(original_string)
+  context 'message contains an alert' do
+    let(:message) { 'Important: Learn how to learn.' }
+
+    it 'returns the message prompt' do
+      expect(destroy_message(message)).to eq 'Important:'
+    end
+
+    it 'modifies its argument be be just the prompt' do
+      expect { destroy_message(message) }.to change { message }.to { 'Important:' }
+    end
+  end
+
+  context 'message does not contain an alert' do
+    it 'returns the whole message' do
+      message = 'No alert here.'
+      expect(destroy_message(message)).to eq 'No alert here.'
+    end
+
+    it 'does not modify its argument' do
+      expect { destroy_message(message) }.to_not change { message }
+    end
   end
 end
