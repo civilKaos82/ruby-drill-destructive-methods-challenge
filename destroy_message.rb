d8113cf0ffb07b295bc73fe61e95654d3445088d
Michael Glaser <m.glaser82@gmail.com>
2017-12-11 06:17:49 -0500

    Refactored #destroy_message to reduce lines of code by removing while loop with nested if/elsif statements.

--- destroy_message.rb
+++ destroy_message.rb
@@ -1,23 +1,36 @@
 def destroy_message(message)
-  i = 0
-  alert_present = false
-  output_string = ""
+  #refactored code
+  colon_index = message.index(':')
 
-  while i < message.length
-    if alert_present
-      i += 1
-    elsif alert_present == false
-      output_string << message[i]
-      if message[i] == ":"
-        alert_present = true
-      end
-      i += 1
-    end
+  if colon_index
+    return message[0..colon_index]
+  elsif !colon_index
+    return message[0..message.length]
   end
-  output_string
+
+  #original code
+  # i = 0
+  # alert_present = false
+  # output_string = ""
+  #
+  # while i < message.length
+  #   if alert_present
+  #     i += 1
+  #   elsif alert_present == false
+  #     output_string << message[i]
+  #     if message[i] == ":"
+  #       alert_present = true
+  #     end
+  #     i += 1
+  #   end
+  # end
+  # output_string
 end
 
 def destroy_message!(message)
+  #refactored code
+
+  #original code
   i = 0
   alert_present = false
 
