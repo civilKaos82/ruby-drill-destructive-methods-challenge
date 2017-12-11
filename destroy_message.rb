51413088534ebaa2ded3e814db956bce9e075619
Michael Glaser <m.glaser82@gmail.com>
2017-12-10 23:06:16 -0500

    Adds functionality to #destroy_message to take in a block and return a string with all characters before the first ':'

--- destroy_message.rb
+++ destroy_message.rb
@@ -1,4 +1,20 @@
 def destroy_message(message)
+  i = 0
+  alert_present = false
+  output_string = ""
+
+  while i < message.length
+    if alert_present
+      i += 1
+    elsif alert_present == false
+      output_string << message[i]
+      if message[i] == ":"
+        alert_present = true
+      end
+      i += 1
+    end
+  end
+  output_string
 end
 
 def destroy_message!(message)
